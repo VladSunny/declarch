7fee40583b25f7fa8c1a454cef62c00e1c5597ff
vladg00dman <vladislavsunny@gmail.com>
2026-08-05 23:05:24 +0300

    UPD gaps

--- dot_config/hypr/hyprland/general.lua
+++ dot_config/hypr/hyprland/general.lua
@@ -5,8 +5,8 @@
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 5,
-        gaps_out = 20,
+        gaps_in  = 3,
+        gaps_out = 5,
 
         border_size = 2,
 
